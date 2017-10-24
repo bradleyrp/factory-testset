d0855c5dd4e0514fd9277933e995f7dc4a6288e4
Ryan Bradley <bradleyrp@gmail.com>
2017-10-24 10:35:50 -0400

    loaded with files

---
+++ docker_config.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env/python
+
+"""
+Consolidated docker settings.
+See the interpreter function below.
+This file should be processed by its own interpreter function.
+"""
+
+__all__ = ['interpreter']
+
+###---DOCKERFILES
+
+dockerfile_jessie = """
+FROM debian:jessie
+"""
+
+dockerfile_debian_start = """
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get update
+RUN apt-get upgrade -y
+RUN apt-get install -y build-essential wget openssh-server apt-utils
+RUN apt-get install -y make cmake git vim screen
+RUN apt-get install -y libfftw3-dev libxml2-dev
+RUN apt-get install -y python
+"""
+
+dockerfile_gromacs = """
+WORKDIR /root
+RUN wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-5.1.2.tar.gz
+RUN tar xvf gromacs-5.1.2.tar.gz
+RUN rm gromacs-5.1.2.tar.gz
+WORKDIR /root/gromacs-5.1.2
+RUN mkdir build
+WORKDIR /root/gromacs-5.1.2/build
+RUN cmake /root/gromacs-5.1.2 -DGMX_USE_RDTSCP=OFF
+RUN make -j 4
+RUN make install
+WORKDIR /root
+RUN echo "source /usr/local/gromacs/bin/GMXRC.bash" >> ~/.bashrc
+"""
+
+dockerfile_debian_python = """
+ARG DEBIAN_FRONTEND=noninteractive
+#---standard packages
+RUN apt-get install -y python-tk
+RUN apt-get install -y python 
+RUN apt-get install -y python-dev 
+RUN apt-get install -y python-numpy 
+RUN apt-get install -y python-numpy-dev
+RUN apt-get install -y python-scipy
+RUN apt-get install -y redis-server
+RUN apt-get install -y python-h5py
+RUN apt-get install -y python-virtualenv
+#---pip packages
+RUN apt-get install -y python-pip
+RUN pip install MDAnalysis
+"""
+
+dockerfile_debian_vmd = """
+ARG DEBIAN_FRONTEND=noninteractive
+RUN apt-get install -y libglu1 libxinerama1 libxi6 libgconf-2-4 imagemagick
+WORKDIR /root/
+COPY vmd-1.9.1.bin.LINUXAMD64.opengl.tar /root/
+RUN tar xvf vmd-1.9.1.bin.LINUXAMD64.opengl.tar
+WORKDIR /root/vmd-1.9.1
+RUN ./configure
+WORKDIR /root/vmd-1.9.1/src
+RUN make install
+"""
+
+dockerfile_debian_ffmpeg = """
+WORKDIR /root/
+RUN echo "deb http://www.deb-multimedia.org jessie main non-free" >> /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get install --force-yes -y deb-multimedia-keyring 
+RUN apt-get update
+RUN apt-get install -y ffmpeg x264
+"""
+
+dockerfile_anaconda2 = """
+COPY Anaconda2-4.2.0-Linux-x86_64.sh /root/
+WORKDIR /root/
+RUN bash Anaconda2-4.2.0-Linux-x86_64.sh -b -p /usr/local/anaconda2
+ENV PATH=/usr/local/anaconda2/bin:$PATH
+RUN pip install numpy
+RUN pip install scipy
+RUN pip install MDAnalysis
+"""
+
+dockerfile_anaconda3 = """
+COPY Anaconda3-4.2.0-Linux-x86_64.sh /root/
+WORKDIR /root/
+RUN bash Anaconda3-4.2.0-Linux-x86_64.sh -b -p /usr/local/anaconda3
+"""
+
+###---REQUIREMENTS
+
+requirements = {
+	'dockerfile_anaconda2':['~/libs/Anaconda2-4.2.0-Linux-x86_64.sh'],
+	'dockerfile_anaconda3':['~/libs/Anaconda3-4.2.0-Linux-x86_64.sh'],
+	'dockerfile_debian_vmd':['~/libs/vmd-1.9.1.bin.LINUXAMD64.opengl.tar'],
+	'dockerfile_factory':['~/libs/'],}
+
+###---SEQUENCES
+
+sequences = [
+	('simple','jessie debian_start gromacs'),
+	('simple_vmd','jessie debian_start gromacs debian_vmd'),
+	('simple_vmd_ffmpeg','jessie debian_start gromacs debian_vmd debian_ffmpeg'),
+	#---basic is an alias for simple with VMD and FFMPEG and serves as a starting point for the factory
+	('basic','jessie debian_start gromacs debian_vmd debian_ffmpeg'),]
+
+###---TESTSETS
+
+testset_sources = ['docker_testset.py']
+
+###---INTERPRETER
+
+def interpreter():
+	"""
+	Interpret this file for the importing function.
+	The docker config for the pier must supply:
+	-- a list of dockerfiles
+	-- requirements on disk for those files
+	-- ????
+	"""
+	import os,re
+	dockerfiles = {}
+	dockerfile_variable_regex = '^dockerfile_(.+)'
+	for key in globals():
+		if re.match(dockerfile_variable_regex,key):
+			dockerfiles[re.match(dockerfile_variable_regex,key).group(1)] = globals()[key]
+	instruct = dict(dockerfiles=dockerfiles,requirements=requirements,sequences=dict(sequences))
+	#---get testsets from external files
+	testset_sources = globals().get('testset_sources',[])
+	#---testset sources update the instruction set sequentially
+	for source in testset_sources: 
+		#---sources are local to this file
+		source_fn = os.path.join(os.path.dirname(__file__),source)
+		if not os.path.isfile(source_fn):
+			raise Exception('missing testset source %s'%source_fn)
+		with open(source_fn) as fp: code = fp.read()
+		mod_this = {}
+		exec(code,mod_this)
+		if 'tests' not in instruct: instruct['tests'] = {}
+		instruct['tests'].update(**mod_this['interpreter']())
+	return instruct
---
+++ docker_testset.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+"""
+TESTSETS FOR FACTORY
+by Ryan Bradley
+on 2017.10.23
+"""
+
+__all__ = ['interpreter']
+
+def interpreter():
+  """
+  Interpret this file for the importing function.
+  """
+  import yaml
+  tests = yaml.load(testsets)
+  counts = dict([(key,sum([set(i)==set(key) for i in tests])) for key in tests])
+  if any([v!=1 for v in counts.values()]):
+    raise Exception('duplicate key sets: %s'%counts)
+  #---the dockerfile key points to variables in this script
+  for key,val in tests.items():
+    if 'script' in val:
+      key_this = val['script']
+      #---you can define the script directly in the test or point to a global variable
+      if key_this in globals(): val['script'] = globals()[key_this]
+  return tests
+
+###---SCRIPTS
+
+test_script_factory_setup = """#!/bin/bash
+set -e
+git clone http://github.com/biophyscode/factory factory
+cd factory
+make nuke sure
+make set species anaconda
+make set anaconda_location=~/Miniconda3-latest-Linux-x86_64.sh
+make set automacs="http://github.com/biophyscode/automacs"
+make set omnicalc="http://github.com/biophyscode/omnicalc"
+cp ~/reqs_conda.yaml .
+cp ~/reqs_pip.txt .
+make set reqs_conda reqs_conda.yaml
+make set reqs_pip reqs_pip.txt
+make setup
+"""
+
+###---TESTSETS
+
+testsets = """
+
+factory setup:
+  notes: |
+    This test will create a blank factory for other tests.
+    It includes custom requirements for anaconda which exclude Mayavi which is not compiling on the debian docker.
+    This test should only be run once, while connection tests can be rerun until the connection is right.
+    After you have a factory and a connection you can run the compute tests below.
+  # which docker to use
+  docker: basic
+  # prevent rerun by logging to docker.json
+  once: True
+  # external location for running the factory
+  where: ~/omicron/PIER
+  # which dockerfile variable to execute
+  script: test_script_factory_setup
+  # custom preparation script
+  collect requirements: >
+    cp ~/libs/Miniconda3-latest-Linux-x86_64.sh ~/omicron/PIER/Miniconda3-latest-Linux-x86_64.sh
+  # local files to copy to where
+  collect files:
+    reqs_conda_factory_setup.yaml: reqs_conda.yaml
+    reqs_pip_factory_setup.txt: reqs_pip.txt
+
+ptdins connect:
+  notes: |
+    Connect to the PtdIns dataset on dark. 
+    Run once unless you change the connection file.
+  docker: basic
+  where: ~/omicron/PIER
+  mounts:
+    ~/omicron/analyze-project-ptdins: analyze-project-ptdins
+    ~/omicron/dataset-project-ptdins: dataset-project-ptdins
+  collect files: 
+    connect_ptdins.yaml: connect_ptdins.yaml
+  script: |
+    #!/bin/bash
+    set -e
+    cp connect_ptdins.yaml factory/connections/
+    cd factory
+    make connect ptdins
+
+ptdins compute:
+  notes: |
+    Run compute on the PtdIns project.
+    Run via: "make config=custom_docker_testsets/docker_config.py test ptdins compute"
+  docker: basic
+  container_site: /root
+  where: ~/omicron/PIER
+  #! all files are collected in the root and then the script has to get them? should they be cleaned?
+  collect files:
+    specs_ptdins_v1.yaml: specs_ptdins_v1.yaml
+  mounts:
+    ~/omicron/analyze-project-ptdins: analyze-project-ptdins
+    ~/omicron/dataset-project-ptdins: dataset-project-ptdins
+  script: |
+    #!/bin/bash
+    set -e
+    cd factory/calc/ptdins
+    cp ~/specs_ptdins_v*.yaml calcs/specs/
+    make compute meta=calcs/specs/specs_ptdins_v1.yaml
+
+ocean connect:
+  docker: basic
+  where: ~/omicron/PIER
+  collect files: 
+    connect_ocean.yaml: connect_ocean.yaml
+  mounts:
+    ~/omicron/analyze-project-ocean/post: analyze-project-ptdins-outside-post
+  collect requirements: |
+    #!/bin/bash
+    set -e
+    cd ~/omicron/PIER/
+    mkdir analyze-project-ocean-post
+    cd analyze-project-ocean-post
+    ln -s /home/rpb/analyze-project-ptdins-outside-post/BMEM_sample.dcd
+    ln -s /home/rpb/analyze-project-ptdins-outside-post/1protein_again2_autopsf.psf
+  script: |
+    #!/bin/bash
+    set -e
+    cp connect_ocean.yaml factory/connections/
+    cd ~/factory
+    make connect ocean
+
+ocean compute:
+  notes: |
+    Compute series for the ocean project.
+    This set is used to reproduce a bunch of bilayer calculations on a small atomistic bilayer.
+  docker: basic
+  where: ~/omicron/PIER
+  collect files:
+    specs_ocean_v1.yaml: specs_ocean_v1.yaml
+    specs_ocean_v2.yaml: specs_ocean_v2.yaml
+    specs_ocean_v3.yaml: specs_ocean_v3.yaml
+  mounts:
+    ~/omicron/analyze-project-ocean/post: analyze-project-ptdins-outside-post
+  script: |
+    #!/bin/bash
+    set -e
+    cd factory/calc/ocean
+    cp ~/specs_ocean_v*.yaml calcs/specs/
+    make compute meta=calcs/specs/specs_ocean_v1.yaml
+    make compute meta=calcs/specs/specs_ocean_v2.yaml
+    make compute meta=calcs/specs/specs_ocean_v3.yaml
+
+"""
